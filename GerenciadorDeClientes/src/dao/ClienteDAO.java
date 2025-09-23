af9c49e87e43d7631eea054deb96142551b54d5b
DinizDDD <davidinizdalevedo9@gmail.com>
2025-09-23 19:58:59 -0300

    make aula 03

---
+++ GerenciadorDeClientes/src/dao/ClienteDAO.java
@@ -0,0 +1,13 @@
+package dao;
+
+import model.Cliente;
+
+public class ClienteDAO {
+
+    public void salvar(Cliente cliente) {
+        // Código para inserir um cliente no banco de dados (INSERT)
+        // Faremos isso na próxima aula!
+    }
+
+    // Outros métodos (listar, atualizar, excluir) virão aqui...
+}
